28f8cdbe1a05ffe959ad6248730a3e809aa6f2a3
EvgenyKuchko <kuchkozhenia@gmail.com>
2023-04-03 15:40:50 +0200

    Initial commit

---
+++ src/test/java/ru/hunt/Request/RequestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hunt.Request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
