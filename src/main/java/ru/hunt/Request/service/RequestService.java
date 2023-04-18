0f2c327a308b1e042506b92d8c137903bff82771
EvgenyKuchko <kuchkozhenia@gmail.com>
2023-04-18 12:26:58 +0200

    created start and stop methods to check requests

--- src/main/java/ru/hunt/Request/service/RequestService.java
+++ src/main/java/ru/hunt/Request/service/RequestService.java
@@ -26,36 +26,29 @@ public class RequestService {
     @Autowired
     private ResourceService resourceService;
 
-    private static final String UNREVIEWED_STATUS = "на рассмотрении";
     private static final String NOT_APPROVED_STATUS = "не одобрено";
     private static final String APPROVED_STATUS = "одобрено";
     private static final String PARTIALLY_APPROVED_STATUS = "частично одобрено";
 
     @Transactional
-    public void checkRequests() {
-        var orders = huntingOrderService.getUnreviewedSortedOrders(statusService.getStatusByName(UNREVIEWED_STATUS));
-        for (HuntingOrder order : orders) {
-            var huntingOrderResources = huntingOrderResourceService.getResourcesByOrder(order);
-            var horStartSize = huntingOrderResources.size();
-            if (!checkType(huntingOrderResources, order)) {
-                continue;
-            }
-            checkSameResource(huntingOrderResources);
-            checkOrderByDate(order.getDate(), huntingOrderResources);
-            checkDistrict(order, huntingOrderResources);
-            checkResourceQuota(huntingOrderResources, order);
-            finalCheck(order, huntingOrderResources, horStartSize);
-        }
+    public void checkRequests(HuntingOrder order) {
+        var huntingOrderResources = huntingOrderResourceService.getResourcesByOrder(order);
+        var horStartSize = huntingOrderResources.size();
+        checkType(huntingOrderResources, order);
+        checkSameResource(huntingOrderResources);
+        checkOrderByDate(order.getDate(), huntingOrderResources);
+        checkDistrict(order, huntingOrderResources);
+        checkResourceQuota(huntingOrderResources, order);
+        finalCheck(order, huntingOrderResources, horStartSize);
     }
 
-    private boolean checkType(List<HuntingOrderResource> hor, HuntingOrder order) {
+    private void checkType(List<HuntingOrderResource> hor, HuntingOrder order) {
         var type = order.getHuntingOrderType().getType();
         if (!huntingOrderResourceService.checkOrderType(hor, type)) {
             huntingOrderService.changeOrderStatus(order, statusService.getStatusByName(NOT_APPROVED_STATUS));
             huntingOrderResourceService.changeResourcesStatusByOrder(order, statusService.getStatusByName(NOT_APPROVED_STATUS));
-            return false;
+            hor.clear();
         }
-        return true;
     }
 
     private void checkOrderByDate(LocalDate date, List<HuntingOrderResource> orderResources) {
@@ -109,11 +102,11 @@ public class RequestService {
                     .distinct()
                     .collect(Collectors.toList());
 
-            for(HuntingOrderResource res : dis) {
+            for (HuntingOrderResource res : dis) {
                 Iterator<HuntingOrderResource> iterator = hor.iterator();
                 while (iterator.hasNext()) {
                     HuntingOrderResource orderResource = iterator.next();
-                    if(orderResource.getId() != res.getId() &&
+                    if (orderResource.getId() != res.getId() &&
                             orderResource.getResource().getName().equals(res.getResource().getName())) {
                         huntingOrderResourceService.changeResourceStatus(orderResource, statusService.getStatusByName(NOT_APPROVED_STATUS));
                         iterator.remove();
